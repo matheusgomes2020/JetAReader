3c810f661bf87c3e107e569cec0fe90055673259
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-23 17:26:10 -0300

    Setting up the Email InputField.

--- app/src/main/java/com/example/jetareader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/login/ReaderLoginScreen.kt
@@ -1,15 +1,17 @@
 package com.example.jetareader.screens.login
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -18,9 +20,14 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetareader.components.EmailInput
 import com.example.jetareader.components.ReaderLogo
 
 @Composable
@@ -58,11 +65,18 @@ fun UserForm() {
 
     val modifier = Modifier
         .height(250.dp)
-        .background( MaterialTheme.colors.background )
-        .verticalScroll( rememberScrollState() )
+        .background(MaterialTheme.colors.background)
+        .verticalScroll(rememberScrollState())
 
     Column( modifier, horizontalAlignment = Alignment.CenterHorizontally ) {
+
+        EmailInput( emailState = email, enabled = true, onAction = KeyboardActions {
+
+            passWordFocusRequest.requestFocus()
+
+        } )
 
     }
 
 }
+
