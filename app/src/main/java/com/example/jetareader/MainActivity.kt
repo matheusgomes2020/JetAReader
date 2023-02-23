add208f8e19c0bf7d6b5e2008236352496cea352
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-23 15:43:05 -0300

    Setting Up Di - Dependency Injection with Hilt.

--- app/src/main/java/com/example/jetareader/MainActivity.kt
+++ app/src/main/java/com/example/jetareader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetareader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetareader.ui.theme.JetAReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.log
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetAReaderTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user[ "firstName" ] = "Jeo"
+                user[ "lastName" ] = "James"
+
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+                    db.collection( "users" )
+                        .add( user )
+                        .addOnSuccessListener {
+
+                            Log.d( "FeeeeB", "onCreate: ${ it.id }" )
+
+                        }.addOnFailureListener {
+
+                            Log.d( "FeeeeB", "onCreate: $it" )
+
+                        }
+
                     Greeting("Android")
                 }
             }
