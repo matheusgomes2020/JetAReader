e8add3f922cbb23d9f36dbf20a72aaa3df3eedba
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-23 16:39:42 -0300

    Creating the Splash Screen.

--- app/src/main/java/com/example/jetareader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/ReaderSplashScreen.kt
@@ -1,12 +1,48 @@
 package com.example.jetareader.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@Preview
 @Composable
-fun ReaderSplashScreen( navController: NavController ) {
+fun ReaderSplashScreen( navController: NavController = NavController( context = LocalContext.current ) ) {
     
-    Text(text = "SplashScreen")
+   Surface(modifier = Modifier
+       .padding(15.dp)
+       .size(330.dp),
+       shape = CircleShape,
+       color = Color.White,
+       border = BorderStroke( width = 2.dp, color = Color.LightGray ) ) {
+
+       Column(
+           modifier = Modifier.padding(1.dp),
+           horizontalAlignment = Alignment.CenterHorizontally,
+           verticalArrangement = Arrangement.Center
+       ) {
+
+           Text( text = "A. Reader", style = MaterialTheme.typography.h3 ,
+               color = Color.Red.copy( alpha = 0.5f ) )
+
+           Spacer( modifier = Modifier.height( 15.dp ) )
+           
+           Text(text = "\"Read. Change. Yourself \"",
+               style = MaterialTheme.typography.h5, color = Color.LightGray
+           )
+
+       }
+
+   }
 
 }
