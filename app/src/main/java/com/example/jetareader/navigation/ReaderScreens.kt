341ce97e2c066930a83a5e6ef28a2e16f4b4319d
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-23 16:11:44 -0300

    Setting up the ReaderScreen Enum & App Routing.

--- app/src/main/java/com/example/jetareader/navigation/ReaderScreens.kt
+++ app/src/main/java/com/example/jetareader/navigation/ReaderScreens.kt
@@ -1,4 +1,34 @@
 package com.example.jetareader.navigation
 
 enum class ReaderScreens {
+
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
+
+    companion object {
+
+        fun fromRoute( route: String ): ReaderScreens
+        = when( route?.substringBefore("/") ) {
+
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            ReaderHomeScreen.name -> ReaderHomeScreen
+            SearchScreen.name -> SearchScreen
+            DetailScreen.name -> DetailScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            null -> ReaderHomeScreen
+            else -> throw java.lang.IllegalArgumentException( "Route $route is not recognize" )
+
+        }
+
+    }
+
 }
