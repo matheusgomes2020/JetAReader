3f1a4a480e52648409987baa52fbeb41d0d2322e
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-23 16:28:11 -0300

    Using the Navigation to Show the SplashScreen.

--- app/src/main/java/com/example/jetareader/MainActivity.kt
+++ app/src/main/java/com/example/jetareader/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetareader.navigation.ReaderNavigation
 import com.example.jetareader.ui.theme.JetAReaderTheme
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
@@ -24,47 +30,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetAReaderTheme {
 
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
-                user[ "firstName" ] = "Jeo"
-                user[ "lastName" ] = "James"
-
-
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                    db.collection( "users" )
-                        .add( user )
-                        .addOnSuccessListener {
-
-                            Log.d( "FeeeeB", "onCreate: ${ it.id }" )
-
-                        }.addOnFailureListener {
-
-                            Log.d( "FeeeeB", "onCreate: $it" )
-
-                        }
-
-                    Greeting("Android")
-                }
+                ReaderApp()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReaderApp(){
+
+    Surface( color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize() ) {
+
+        Column( verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+            ReaderNavigation()
+
+        }
+
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetAReaderTheme {
-        Greeting("Android")
+
     }
 }
--- app/src/main/java/com/example/jetareader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/ReaderSplashScreen.kt
@@ -1,11 +1,12 @@
 package com.example.jetareader.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderSplashScreen( navController: NavController ) {
-
-
+    
+    Text(text = "SplashScreen")
 
 }
