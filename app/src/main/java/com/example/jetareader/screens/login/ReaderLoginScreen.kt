62f4c4a150a803a6bf09fab872c683fdff4787d5
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-24 15:31:06 -0300

    Showing Login Form and Login Button.

--- app/src/main/java/com/example/jetareader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/login/ReaderLoginScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -18,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -66,12 +68,12 @@ fun UserForm(
 
         ) {
 
-    val email = rememberSaveable { mutableStateOf("" ) }
-    val passWord = rememberSaveable { mutableStateOf("" ) }
-    val passWordVisibility = rememberSaveable { mutableStateOf( false ) }
+    val email = rememberSaveable { mutableStateOf("") }
+    val passWord = rememberSaveable { mutableStateOf("") }
+    val passWordVisibility = rememberSaveable { mutableStateOf(false) }
     val passWordFocusRequest = FocusRequester.Default
     val keyboardController = LocalSoftwareKeyboardController.current
-    val valid = remember( email.value, passWord.value ) {
+    val valid = remember(email.value, passWord.value) {
 
         email.value.trim().isNotEmpty() && passWord.value.trim().isNotEmpty()
 
@@ -82,30 +84,62 @@ fun UserForm(
         .background(MaterialTheme.colors.background)
         .verticalScroll(rememberScrollState())
 
-    Column( modifier, horizontalAlignment = Alignment.CenterHorizontally ) {
+    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
 
-        EmailInput( emailState = email, enabled = !loading, onAction = KeyboardActions {
+        EmailInput(
+            emailState = email, enabled = !loading,
+            onAction = KeyboardActions {
 
-            passWordFocusRequest.requestFocus()
+                passWordFocusRequest.requestFocus()
 
-        }, )
+            },
+        )
 
         PassWordInput(
-            modifier = Modifier.focusRequester( passWordFocusRequest ),
+            modifier = Modifier.focusRequester(passWordFocusRequest),
             passWordState = passWord,
             labelId = "Password",
             enabled = !loading,
             passWordVisibility = passWordVisibility,
             onAction = KeyboardActions {
 
-                if ( !valid ) return@KeyboardActions
-                onDone( email.value.trim(), passWord.value.trim() )
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), passWord.value.trim())
 
-            } )
+            })
+
+        SubmitButton(
+
+            textId = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid
+        ) {
+
+            onDone(email.value.trim(), passWord.value.trim())
+        }
+
 
     }
+}
 
+@Composable
+fun SubmitButton(textId: String ,
+                  loading: Boolean ,
+                  validInputs: Boolean,
+                onClick: () -> Unit ) {
 
+    Button(
+        onClick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape) {
+
+        if ( loading ) CircularProgressIndicator( modifier = Modifier.size( 25.dp ) )
+        else Text( text = textId, modifier = Modifier.padding( 5.dp ) )
+
+        }
 
 }
 
