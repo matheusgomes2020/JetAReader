47ffecf8bf605067b46cad3d3f05118323b11cd1
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-03-01 15:40:37 -0300

    Creating the BookSearch ViewModel.

--- app/src/main/java/com/example/jetareader/screens/search/BookSearchViewModel.kt
+++ app/src/main/java/com/example/jetareader/screens/search/BookSearchViewModel.kt
@@ -2,10 +2,13 @@ package com.example.jetareader.screens.search
 
 import android.util.Log
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetareader.data.DataOrException
+import com.example.jetareader.data.Resource
 import com.example.jetareader.model.Item
 import com.example.jetareader.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,6 +20,8 @@ import javax.inject.Inject
 class BookSearchViewModel @Inject constructor( private val repository: BookRepository ):
             ViewModel(){
 
+    var list: List<Item> by mutableStateOf( listOf() )
+
     val listOfBooks: MutableState<DataOrException<List<Item>, Boolean, Exception>>
     = mutableStateOf( DataOrException( null, true, Exception( "" ) ) )
 
@@ -24,9 +29,15 @@ class BookSearchViewModel @Inject constructor( private val repository: BookRepos
         searchBooks( "android" )
     }
 
+    private fun loadBooks() {
+
+        searchBooks( "android" )
+
+    }
+
      fun searchBooks(query: String) {
 
-        viewModelScope.launch  {
+        viewModelScope.launch( Dispatchers.Default )  {
 
             if ( query.isEmpty() ) {
 
@@ -34,14 +45,34 @@ class BookSearchViewModel @Inject constructor( private val repository: BookRepos
 
             }
 
-            listOfBooks.value.loading = true
-            listOfBooks.value = repository.getBooks( query )
+            try {
 
-            Log.d( "Search", "searchBooks: ${ listOfBooks.value.data }")
+                when( val response = repository.getBooks( query ) ) {
 
-            if ( listOfBooks.value.data.toString().isNotEmpty() ) listOfBooks.value.loading = false
+                    is Resource.Success -> {
+
+                        list = response.data!!
+
+                    }
+
+                    is Resource.Error -> {
+
+                        Log.e( "Network", "searchBooks: Failed getting books", )
+
+                    }
+
+                    else -> {  }
+
+                }
+
+            }catch ( exception: Exception ) {
+
+                Log.d( "Network", "searchBooks: ${ exception.message.toString() }")
+
+            }
 
         }
 
     }
+
  }
