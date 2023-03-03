cd768152c3fd01176beda8e2417539f429d2d497
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-03-02 21:18:27 -0300

    Adding a Note TextField.

--- app/src/main/java/com/example/jetareader/screens/update/ReaderBookUpdateScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/update/ReaderBookUpdateScreen.kt
@@ -3,24 +3,33 @@ package com.example.jetareader.screens.update
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import com.example.jetareader.components.InputField
 import com.example.jetareader.components.ReaderAppBar
 import com.example.jetareader.data.DataOrException
 import com.example.jetareader.model.MBook
@@ -94,13 +103,59 @@ fun BookUpdateScreen(navController: NavController,
 @Composable
 fun ShowSimpleForm(book: MBook, navController: NavController) {
 
-    SimpleForm()
+    val notesText = remember {
+
+        mutableStateOf( "" )
+
+    }
+
+    SimpleForm( defaultValue = if ( book.notes.toString().isNotEmpty() ) book.notes.toString()
+                else "No thoughts available." ) { note ->
+
+        notesText.value = note
+
+    }
 
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun SimpleForm() {
-    TODO("Not yet implemented")
+fun SimpleForm(
+    modifier: Modifier = Modifier,
+    loading: Boolean = false,
+    defaultValue: String = "Great Book!",
+    onSearch: (String) -> Unit
+            ) {
+
+    Column() {
+
+        val textFieldValue = rememberSaveable { mutableStateOf( defaultValue ) }
+
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        val valid = remember( textFieldValue.value ) { textFieldValue.value.trim().isNotEmpty() }
+
+        InputField(
+            modifier
+                .fillMaxWidth()
+                .height(140.dp)
+                .padding(3.dp)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp),
+            valueState =  textFieldValue ,
+            labelId = "Enter Your thoughts" ,
+            enabled = true,
+        onAction = KeyboardActions{
+
+            if ( !valid ) return@KeyboardActions
+            onSearch( textFieldValue.value.trim() )
+            keyboardController?.hide()
+
+        }
+        )
+
+    }
+
 }
 
 @Composable
