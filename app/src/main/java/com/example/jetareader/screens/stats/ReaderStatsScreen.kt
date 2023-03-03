6d57f2453512d7d7207a643d8d4558a84230623d
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-03-03 14:55:12 -0300

    Finishing the Stats Screen.

--- app/src/main/java/com/example/jetareader/screens/stats/ReaderStatsScreen.kt
+++ app/src/main/java/com/example/jetareader/screens/stats/ReaderStatsScreen.kt
@@ -1,7 +1,12 @@
 package com.example.jetareader.screens.stats
 
 import android.annotation.SuppressLint
+import android.media.audiofx.DynamicsProcessing.Mbc
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -10,11 +15,17 @@ import androidx.compose.material.icons.sharp.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.jetareader.components.ReaderAppBar
+import com.example.jetareader.model.Item
 import com.example.jetareader.model.MBook
+import com.example.jetareader.navigation.ReaderScreens
 import com.example.jetareader.screens.home.HomeScreenViewModel
+import com.example.jetareader.screens.search.BookRow
 import com.google.firebase.auth.FirebaseAuth
 import java.util.*
 
@@ -102,7 +113,107 @@ fun ReaderStatsScreen(navController: NavController, viewModel: HomeScreenViewMod
                     }
 
                 }
+
+                if ( viewModel.data.value.loading == true ) {
+
+                    LinearProgressIndicator()
+
+                }else {
+
+                    Divider()
+
+                    LazyColumn( modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    contentPadding = PaddingValues( 16.dp ) ) {
+
+                        //filter books by finished ones
+
+                        val readBooks: List<MBook> = if ( !viewModel.data.value.data.isNullOrEmpty() ) {
+
+                            viewModel.data.value.data!!.filter { mBook ->
+
+                                ( mBook.userId == currentUser?.uid ) && ( mBook.finishedReading != null )
+
+                            }
+
+                        }else {
+
+                            emptyList()
+
+                        }
+
+                        items( items = readBooks ) { book ->
+
+                            BookRowStats(book = book)
+
+                        }
+
+                    }
+
+
+                }
+
             }
+        }
+
+    }
+
+}
+
+@Composable
+fun BookRowStats(
+    book: MBook, ) {
+    Card(modifier = Modifier
+        .clickable {
+            //navController.navigate(ReaderScreens.DetailScreen.name + "/${book.id}")
+        }
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(3.dp),
+        shape = RectangleShape,
+        elevation = 7.dp) {
+        Row(modifier = Modifier.padding(5.dp),
+            verticalAlignment = Alignment.Top) {
+
+            val imageUrl: String = if(book.photoUrl.toString().isEmpty())
+                "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=80&q=80"
+            else {
+                book.photoUrl.toString()
+            }
+            Image(
+                painter = rememberImagePainter(data = imageUrl),
+                contentDescription = "book image",
+                modifier = Modifier
+                    .width(80.dp)
+                    .fillMaxHeight()
+                    .padding(end = 4.dp),
+            )
+
+            Column {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(text =  "Author: ${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                    style = MaterialTheme.typography.caption)
+
+                Text(text =  "Date: ${book.publishedDate}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                    style = MaterialTheme.typography.caption)
+
+                Text(text =  "${book.categories}",
+                    overflow = TextOverflow.Clip,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                    style = MaterialTheme.typography.caption)
+
+
+
+
+
+
+            }
+
         }
 
     }
